cfdd9d70b4309f06fe77caa1a19dcd9297904e24
IJ Baig <151609921+IJBaig@users.noreply.github.com>
2026-07-15 23:26:02 +0500

    Update screenshot filename and git add command

--- update_badge.sh
+++ update_badge.sh
@@ -3,16 +3,16 @@ set -e
 URL="https://tryhackme.com/api/v2/badges/public-profile?userPublicId=2577592" 
 
 chromium --headless --disable-gpu --no-sandbox \
-  --screenshot=badge.png \
+  --screenshot=image.png \
   --window-size=330,175 \
   --force-device-scale-factor=2 \
   "$URL"
   
-convert badge.png -trim +repage badge.png 
+convert image.png -trim +repage image.png 
 
 git config user.name "IJBaig" 
 git config user.email "ijbaig53@gmail.com" 
-git add badge.png 
+git add image.png 
 git commit -m "Update THM badge $(date +%F)" || exit 0 
 
 git push https://$GH_TOKEN@github.com/IJBaig/Random-Empty.git HEAD:main
